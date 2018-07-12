ff691d38102b575e8e6684e8e8e5535848cf4291
mak <kochen.michael@gmail.com>
2018-07-11 19:15:19 -0500

    update

--- run_HB.py
+++ run_HB.py
@@ -1,9 +1,15 @@
 
 from HypBuilder import ModelAssembler
 
-ModelAssembler('HB_library.txt', 'earm_example.csv')
+# to run an example just uncomment the appropriate line and run this file
+
+# ModelAssembler('HB_library.txt', 'earm_example.csv')
 # ModelAssembler('HB_library.txt', 'sequence_example.csv')
 # ModelAssembler('HB_library.txt', 'competitive_binding_example.csv')
 # ModelAssembler('HB_library.txt', 'optional_reactions_example.csv')
 # ModelAssembler('HB_library.txt', 'optional_reactions_with_data_node_example.csv')
 # ModelAssembler('HB_library.txt', 'initial_values_example.csv')
+
+# ModelAssembler('HB_library.txt', 'earm.csv')
+# ModelAssembler('HB_library.txt', 'caspase_pathway.csv')
+# ModelAssembler('HB_library.txt', 'momp_pathway.csv')
