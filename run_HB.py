074c8e1b0c0b2d5deabd393d9b3e7ec3d99c07d6
mak <kochen.michael@gmail.com>
2018-07-11 19:16:47 -0500

    update

--- run_HB.py
+++ run_HB.py
@@ -10,6 +10,3 @@ from HypBuilder import ModelAssembler
 # ModelAssembler('HB_library.txt', 'optional_reactions_with_data_node_example.csv')
 # ModelAssembler('HB_library.txt', 'initial_values_example.csv')
 
-# ModelAssembler('HB_library.txt', 'earm.csv')
-# ModelAssembler('HB_library.txt', 'caspase_pathway.csv')
-# ModelAssembler('HB_library.txt', 'momp_pathway.csv')
