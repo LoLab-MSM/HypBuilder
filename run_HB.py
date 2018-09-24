2619eba117fe1abfa73e7f5d3f75f02edc426c1a
mialko <kochen.michael@gmail.com>
2018-09-24 00:16:19 -0500

    update

--- run_HB.py
+++ run_HB.py
@@ -7,6 +7,7 @@ from HypBuilder import ModelAssembler
 # ModelAssembler('HB_library.txt', 'sequence_example.csv')
 # ModelAssembler('HB_library.txt', 'competitive_binding_example.csv')
 # ModelAssembler('HB_library.txt', 'optional_reactions_example.csv')
+# ModelAssembler('HB_library.txt', 'grouped_reactions_example.csv')
 # ModelAssembler('HB_library.txt', 'initial_binding_example.csv')
 # ModelAssembler('HB_library.txt', 'optional_reactions_with_data_node_example.csv')
 # ModelAssembler('HB_library.txt', 'initial_values_example.csv')
@@ -14,5 +15,3 @@ from HypBuilder import ModelAssembler
 # ModelAssembler('HB_library.txt', 'optional_sequence_example.csv')
 # ModelAssembler('HB_library.txt', 'dwdc_example.csv')
 # ModelAssembler('HB_library.txt', 'dwdc_sequence_example.csv')
-
-
