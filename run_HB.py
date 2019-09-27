cbf3bea3feab2e7a7eb9d070dd4d9cea6451cfb7
mialko <kochen.michael@gmail.com>
2019-09-27 11:31:20 -0500

    Update run_HB.py

--- run_HB.py
+++ run_HB.py
@@ -3,13 +3,12 @@ from HypBuilder import ModelAssembler
 
 # to run an example just uncomment the appropriate line and run this file
 
-ModelAssembler('HB_library.txt', 'earm_example.csv')
-ModelAssembler('HB_library.txt', 'sequence_example.csv')
-ModelAssembler('HB_library.txt', 'optional_reactions_example.csv')
-ModelAssembler('HB_library.txt', 'grouped_reactions_example.csv')
-ModelAssembler('HB_library.txt', 'initial_binding_example.csv')
-ModelAssembler('HB_library.txt', 'optional_reactions_with_data_node_example.csv')
-ModelAssembler('HB_library.txt', 'initial_values_example.csv')
-ModelAssembler('HB_library.txt', 'dwdc_example.csv')
-ModelAssembler('HB_library.txt', 'dwdc_sequence_example.csv')
-ModelAssembler('HB_library.txt', 'binding_sites_example.csv')
+# ModelAssembler('HB_library.txt', 'earm_example.csv')
+# ModelAssembler('HB_library.txt', 'sequence_example.csv')
+# ModelAssembler('HB_library.txt', 'optional_reactions_example.csv')
+# ModelAssembler('HB_library.txt', 'grouped_reactions_example.csv')
+# ModelAssembler('HB_library.txt', 'initial_binding_example.csv')
+# ModelAssembler('HB_library.txt', 'optional_reactions_with_data_node_example.csv')
+# ModelAssembler('HB_library.txt', 'initial_values_example.csv')
+# ModelAssembler('HB_library.txt', 'dwdc_example.csv')
+# ModelAssembler('HB_library.txt', 'binding_sites_example.csv')
