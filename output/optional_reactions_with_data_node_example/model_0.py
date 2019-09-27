6cb4f7358814d8c446626893dc731fd96a154cae
mialko <kochen.michael@gmail.com>
2019-09-27 00:04:58 -0500

    update

---
+++ output/optional_reactions_with_data_node_example/model_0.py
@@ -0,0 +1,23 @@
+# exported from PySB model 'model'
+
+from pysb import Model, Monomer, Parameter, Expression, Compartment, Rule, Observable, Initial, MatchOnce, Annotation, ANY, WILD
+
+Model()
+
+Monomer('E', ['D'])
+Monomer('D', ['E'])
+
+Parameter('inhibition_0_D_inhibitor_E_inh_target_2kf', 1.0)
+Parameter('inhibition_0_D_inhibitor_E_inh_target_1kr', 1.0)
+Parameter('E_0', 1.0)
+Parameter('D_0', 1.0)
+
+Observable('E_obs', E())
+Observable('D_obs', D())
+
+Rule('inhibition_0_D_inhibitor_E_inh_target', D(E=None) + E(D=None) | D(E=1) % E(D=1), inhibition_0_D_inhibitor_E_inh_target_2kf, inhibition_0_D_inhibitor_E_inh_target_1kr)
+
+Initial(E(D=None), E_0)
+Initial(D(E=None), D_0)
+
+
